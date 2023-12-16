0524a5711cc5b8ae62a670e18336a6e6d82aceb4
Gleb Kovalev <gleb.kovalev31@gmail.com>
2023-12-16 23:27:16 +0300

    Добавлены функции расчёта периметра и площади треугольника

---
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -0,0 +1,12 @@
+package ru.stqa.geometry.figures;
+
+/*public record Triangle(
+        double a,
+        double b,
+        double c
+) {
+
+    public static void main(String[] args) {
+        System.out.println("Периметр треуголника = " + (a + b + c));
+    }
+}*/
