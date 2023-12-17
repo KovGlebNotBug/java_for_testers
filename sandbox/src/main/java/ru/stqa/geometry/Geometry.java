d4d7828a3703112ff3a0c944379251cdb0003396
Gleb Kovalev <gleb.kovalev31@gmail.com>
2023-12-18 01:47:08 +0300

    Выполнена структуризация кода

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -1,37 +1,14 @@
 package ru.stqa.geometry;
 
+import ru.stqa.geometry.figures.Triangle;
+
 public class Geometry {
     public static void main(String[] args) {
-        printTrianglePerimeter(3,4,5);
+        Triangle.printTrianglePerimeter(3,4,5);
 
-        printTriangleArea(6, 5, 2.2);
-
-    }
-
-    private static void printTriangleArea(double a, double b, double c) {
-        var p = semiperimeter(a, b, c);
-        var s = triangleArea(a, b, c, p);
-        /*
-        //If you want to round the output
-        var s1 = String.format("%.2f", s);
-        */
-        System.out.println("Площядь треуголника с сторонами " + a + ", " + b + " и " + c + " = " + s);
-    }
-
-    private static double triangleArea(double a, double b, double c, double p) {
-        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
-    }
-    private static double semiperimeter(double a, double b, double c) {
-        return (a + b + c) / 2;
+        Triangle.printTriangleArea(6, 5, 2.2);
 
     }
 
 
-    static void printTrianglePerimeter(double sideA, double b, double c){
-        System.out.println("Периметр треуголника с сторонами " + sideA + ", " + b + " и " + c + " = " + trianglePerimeter(sideA, b, c));
-    }
-
-    private static double trianglePerimeter(double a, double b, double c) {
-        return a + b + c;
-    }
 }
--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,12 +1,29 @@
 package ru.stqa.geometry.figures;
 
-/*public record Triangle(
-        double a,
-        double b,
-        double c
-) {
+public class Triangle {
 
-    public static void main(String[] args) {
-        System.out.println("Периметр треуголника = " + (a + b + c));
+    public static void printTriangleArea(double a, double b, double c) {
+        var p = semiperimeter(a, b, c);
+        var s = triangleArea(a, b, c, p);
+        String textArea = String.format("Площядь треуголника с сторонами %.1f / %.1f / %.1f = %.2f", a, b, c, s);
+        System.out.println(textArea);
     }
-}*/
+
+    private static double triangleArea(double a, double b, double c, double p) {
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    private static double semiperimeter(double a, double b, double c) {
+        return (a + b + c) / 2;
+
+    }
+
+    public static void printTrianglePerimeter(double sideA, double b, double c){
+        String textPerimeter = String.format("Периметр треуголника с сторонами %.1f / %.1f / %.1f = %.2f" , sideA, b, c, trianglePerimeter(sideA, b, c));
+        System.out.println(textPerimeter);
+    }
+
+    private static double trianglePerimeter(double a, double b, double c) {
+        return a + b + c;
+    }
+}
