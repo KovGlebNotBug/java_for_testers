846c3ca6b091f58b9983658c4c1bc1cf759aa02b
Gleb Kovalev <gleb.kovalev31@gmail.com>
2023-12-21 01:20:16 +0300

    Комит для вопроса

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,21 +1,19 @@
 package ru.stqa.geometry.figures;
 
+import java.math.RoundingMode;
+
 public class Triangle {
 
     public static void printTriangleArea(double a, double b, double c) {
-        var p = semiperimeter(a, b, c);
-        var s = triangleArea(a, b, c, p);
-        String textArea = String.format("Площядь треуголника с сторонами %.1f / %.1f / %.1f = %.2f", a, b, c, s);
+        // Вопрос! Если использую для ввывода эту строку, то результат расчёта = 5.28, при этом в тесте расчёт = 5.279999999999998
+        // String textArea = String.format("Площядь треуголника с сторонами %.1f / %.1f / %.1f = %f ", a, b, c, triangleArea(a,b,c));
+        String textArea = String.format("Площядь треуголника с сторонами %.1f / %.1f / %.1f = " + triangleArea(a,b,c), a, b, c);
         System.out.println(textArea);
     }
 
-    public static double triangleArea(double a, double b, double c, double p) {
+    public static double triangleArea(double a, double b, double c) {
+        var p = (a + b + c) / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
-    }
-
-    public static double semiperimeter(double a, double b, double c) {
-        return (a + b + c) / 2;
-
     }
 
     public static void printTrianglePerimeter(double sideA, double b, double c){
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -6,21 +6,14 @@ import org.junit.jupiter.api.Test;
 public class TriangleTests {
 
     @Test
-    void canCalculateArea () {
-        var a = 6.;
-        var b = 5.;
-        var c = 2.2;
-        var result1 = Triangle.semiperimeter(a,b,c);
-        Assertions.assertEquals (6.6, result1);
-        var result2 = Triangle.triangleArea(a,b,c,result1);
-        Assertions.assertEquals(5.279999999999998,result2);
-
-    }
-
-    @Test
     void canCalculatePerimeter () {
         var result = Triangle.trianglePerimeter(3,4,5);
         Assertions.assertEquals(12,result);
+    }
 
+    @Test
+    void canCalculateArea () {
+        var result = Triangle.triangleArea(5.,6.,2.2);
+        Assertions.assertEquals(5.279999999999998, result);
     }
 }
