70b31eb118742a51dc1481e6dfe60fee75f67d06
Gleb Kovalev <gleb.kovalev31@gmail.com>
2024-01-04 04:17:43 +0300

    Перенес вспомогательные  методы для тестов в TestBase

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -1,74 +1,19 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import tests.TestBase;
 
-public class ContactCreationTests {
-    private static WebDriver driver;
-
-    @BeforeEach
-    public void setUp() {
-        if (driver == null) {
-            driver = new FirefoxDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
-            driver.manage().window().setSize(new Dimension(958, 680));
-            driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).sendKeys("secret");
-            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        }
-    }
-
-    public boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException exception) {
-            return false;
-        }
-    }
+public class ContactCreationTests extends TestBase {
 
 
     @Test
     public void canCreateContact() {
-        if (!isElementPresent(By.linkText("add new"))) {
-            driver.get("http://localhost/addressbook/");
-        }
-        driver.findElement(By.linkText("add new")).click();
-        driver.findElement(By.name("firstname")).click();
-        driver.findElement(By.name("firstname")).sendKeys("first name");
-        driver.findElement(By.name("lastname")).click();
-        driver.findElement(By.name("lastname")).sendKeys("last name");
-        driver.findElement(By.name("address")).click();
-        driver.findElement(By.name("address")).sendKeys("address test");
-        driver.findElement(By.name("home")).click();
-        driver.findElement(By.name("home")).sendKeys("phone test 123");
-        driver.findElement(By.name("email")).click();
-        driver.findElement(By.name("email")).sendKeys("email@example.com");
-        driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
-        driver.findElement(By.linkText("home page")).click();
+        openContactsPage();
+        createContact("first name", "last name", "address test", "phone test 123", "email@example.com" );
     }
 
     @Test
     public void canCreateContactWithEmptyData() {
-        if (!isElementPresent(By.linkText("add new"))) {
-        driver.get("http://localhost/addressbook/");
+        openContactsPage();
+        createContact("","","","","");
     }
-        driver.findElement(By.linkText("add new")).click();
-        driver.findElement(By.name("firstname")).click();
-        driver.findElement(By.name("firstname")).sendKeys("");
-        driver.findElement(By.name("lastname")).click();
-        driver.findElement(By.name("lastname")).sendKeys("");
-        driver.findElement(By.name("address")).click();
-        driver.findElement(By.name("address")).sendKeys("");
-        driver.findElement(By.name("home")).click();
-        driver.findElement(By.name("home")).sendKeys("");
-        driver.findElement(By.name("email")).click();
-        driver.findElement(By.name("email")).sendKeys("");
-        driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
-        driver.findElement(By.linkText("home page")).click();
-    }
+
 }
--- addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
@@ -1,36 +1,15 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import tests.TestBase;
 
-public class ContactRemovalTests {
-    private WebDriver driver;
-
-    @BeforeEach
-    public void setUp() {
-        if (driver == null) {
-            driver = new FirefoxDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
-            driver.manage().window().setSize(new Dimension(958, 680));
-            driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).sendKeys("secret");
-            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        }
-    }
-
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
-    }
+public class ContactRemovalTests extends TestBase {
 
     @Test
     public void canRemoveContact() {
-        driver.findElement(By.linkText("home")).click();
-        driver.findElement(By.name("selected[]")).click();
-        driver.findElement(By.xpath("//input[@value='Delete']")).click();
+        openContactsPage();
+        if (!isContactPresent()) {
+            createContact("first name", "last name", "address test", "phone test 123", "email@example.com");
+        }
+        removeContact();
     }
+
 }
