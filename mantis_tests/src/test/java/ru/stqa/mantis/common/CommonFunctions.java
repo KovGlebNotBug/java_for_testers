33a250a595314ed74a7064d2f86cb870bd6a4233
Gleb Kovalev <gleb.kovalev31@gmail.com>
2024-02-08 00:59:55 +0300

    добавлен почтовый сервер james и тест к нему

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/common/CommonFunctions.java
@@ -0,0 +1,28 @@
+package ru.stqa.mantis.common;
+
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CommonFunctions {
+    public static String randomString(int n) {
+        var rnd = new Random();
+        /**
+         * пример функционального стиля написания кода.
+         * в императивном стиле, код выглядел так
+         * var result = "";
+         * for (int i = 0; i < n; i++) {
+         *     result = result + (char)('a' + rnd.nextInt(26));
+         * }
+         *   return result;
+         */
+        Supplier<Integer> randomNumbers = () -> rnd.nextInt(26);
+        var result = Stream.generate(randomNumbers)
+                .limit(n)
+                .map(i -> 'a' + i)
+                .map(Character::toString)
+                .collect(Collectors.joining());
+        return result;
+    }
+}
